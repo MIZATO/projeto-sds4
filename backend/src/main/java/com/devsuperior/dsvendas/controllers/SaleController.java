5d1aaa492b4615e58b81a0fbc4473fe3dfe4a34e
MIZATO <marlon.santos27@gmail.com>
2021-09-11 04:10:35 -0300

    Group by Search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -1,8 +1,6 @@
 package com.devsuperior.dsvendas.controllers;
 
-
-
-
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.service.SaleService;
 
 @RestController
@@ -28,4 +28,15 @@ public class SaleController {
 		return ResponseEntity.ok(list);
 	}
 	
+	@GetMapping(value ="/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGrupedBySeller(){
+		List<SaleSumDTO> list = service.amountGrupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value ="/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGrupedBySeller(){
+		List<SaleSuccessDTO> list = service.successGrupedBySeller();
+		return ResponseEntity.ok(list);
+	}
 }
